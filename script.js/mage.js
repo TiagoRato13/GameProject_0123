9ed6bb5c45f14de0968082c678f75fa354ed40ea
DuarteFranca <duartefranca_@outlook.com>
2023-01-31 11:06:15 +0000

    merge

--- script.js/mage.js
+++ script.js/mage.js
@@ -3,29 +3,7 @@ class Mage extends Component{
         super(x, y, w, h)
         this.health = 10;
         this.strength = 5;
-        this.intervalId = null;
-        this.frames = 0;
-        this.animation = 0;
 
-        //IDLE
-        const idle1 = new Image();
-        const idle2 = new Image();
-        const idle3 = new Image();
-        const idle4 = new Image();
-        const idle5 = new Image();
-        const idle6 = new Image();
-        const idle7 = new Image();
-
-        idle1.src = "/docs/assets/Images/Lightning Mage/idle/Idle1.png";
-        idle2.src = "/docs/assets/Images/Lightning Mage/idle/Idle2.png";
-        idle3.src = "/docs/assets/Images/Lightning Mage/idle/Idle3.png";
-        idle4.src = "/docs/assets/Images/Lightning Mage/idle/Idle4.png";
-        idle5.src = "/docs/assets/Images/Lightning Mage/idle/Idle5.png";
-        idle6.src = "/docs/assets/Images/Lightning Mage/idle/Idle6.png";
-        idle7.src = "/docs/assets/Images/Lightning Mage/idle/Idle7.png";
-
-
-        //RUN
          const run1 = new Image();
          const run2 = new Image();
          const run3 = new Image();
@@ -34,7 +12,6 @@ class Mage extends Component{
          const run6 = new Image();
          const run7 = new Image();
          const run8 = new Image();
-         
 
         //2-
         /* run1.addEventListener("load", () => {}) */
@@ -47,31 +24,14 @@ class Mage extends Component{
         run7.src = "./docs/assets/Images/Lightning Mage/run/Run7.png";
         run8.src = "./docs/assets/Images/Lightning Mage/run/Run8.png";
 
-        this.image = idle1;
+        this.image = run3
         this.mageRun = [run1, run2, run3, run4, run5, run6, run7, run8];
-        this.mageIdle = [idle1, idle2, idle3, idle4, idle5, idle6, idle7];
-    }
 
-    start(){
-        this.intervalId = setInterval(this.update, 1000 / 60);
-    }
-
-    update = () => {
-        this.frames++;
-        this.draw();
-        this.clear()
-    }
-
-    clear() {
-        ctx.clearRect(0, 0, this.width, this.height);
     }
 
     draw(){
-        if(this.frames % 12 === 0) {
-            this.animation = (this.animation + 1) % this.mageIdle.length
-        } 
-
-        ctx.drawImage(this.mageIdle[this.animation], this.x, this.y, this.w, this.h);
+        ctx.drawImage(this.image, this.x, this.y, this.w, this.h);
+        /* this.ctx.drawImage() */
     }
 
     moveRight(){
